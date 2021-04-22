02d1c9c20365a703e5c4e4a65ff63d99f2df771f
karenm08 <wmok14@my.bcit.ca>
2021-04-21 22:50:52 -0700

    style ask a lawyer text

--- src/layouts/Register.js
+++ src/layouts/Register.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from '../images/checkmark logo.PNG';
 import { makeStyles } from '@material-ui/core/styles';
+import HelpIcon from '@material-ui/icons/Help';
 
 export default function Register() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <div className={classes.text}>Ask a lawyer</div>
+      <div className={classes.lawyerText}>
+        <HelpIcon /> <span className={classes.text}>Ask a lawyer</span>
+      </div>
       <div className={classes.logo}>
         <img src={logo} alt="Logo" />
       </div>
@@ -21,14 +24,17 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+  lawyerText: {
+    display: 'flex',
+    alignItems: 'center',
     color: 'red',
     fontWeight: 'bold',
     position: 'absolute',
-    right: 10,
+    right: 20,
     top: 10,
   },
   logo: {
-    marginTop: '10%',
+    marginTop: '20%',
   },
+  text: { marginLeft: '5px' },
 }));
