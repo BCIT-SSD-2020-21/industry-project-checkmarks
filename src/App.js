acded7c27ff3830b37e61567296259e25280abc1
Kalvin Tang <ktang82@my.bcit.ca>
2021-04-22 00:35:45 -0700

    added text in trademark app page #19

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TrademarkApp from './layouts/TrademarkApp';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <TrademarkApp />;
 }
 
 export default App;
---
+++ src/layouts/TrademarkApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AskALawyer from '../components/AskALawyer';
+
+const TrademarkApp = () => {
+  return (
+    <div>
+      <AskALawyer />
+      <h1>trademark page</h1>
+    </div>
+  );
+};
+
+export default TrademarkApp;
