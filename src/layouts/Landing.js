626cf6d258df8ebe571663fed143535a079f09d2
veeepi <vpreduna90@gmail.com>
2021-04-23 13:14:05 -0700

    LandingPage display logic (Login/Search); #33

--- src/layouts/Landing.js
+++ src/layouts/Landing.js
@@ -1,22 +1,69 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import logo from '../images/checkmark logo.PNG';
 import { makeStyles } from '@material-ui/core/styles';
 import HelpIcon from '@material-ui/icons/Help';
-import { Box, Typography, Tabs, Tab, Card } from '@material-ui/core';
+import {
+    Box,
+    Typography,
+    Tabs,
+    Tab,
+    Card,
+    IconButton,
+    Button,
+} from '@material-ui/core';
+import SearchTwoToneIcon from '@material-ui/icons/SearchTwoTone';
 import IndividualForm from '../components/RegisterPage/IndividualForm';
 import OrganizationForm from '../components/RegisterPage/OrganizationForm';
 import HeaderBanner from '../components/HeaderBanner';
+import UserDashboard from '../components/UserDashboard';
 import TrademarkSearch from '../components/LandingPage/TrademarkSearch';
 import Login from '../components/LandingPage/Login';
 
 export default function Landing() {
+    const history = useHistory();
+    const [authenticated, setAuthenticated] = useState(false);
     const [loggingIn, setLoggingIn] = useState(false);
+    const [searching, setSearching] = useState(false);
 
     return (
         <Box>
-            <HeaderBanner />
-            <TrademarkSearch />
-            <Login />
+            {authenticated ? <UserDashboard /> : <HeaderBanner />}
+            {searching ? (
+                <TrademarkSearch />
+            ) : (
+                <IconButton
+                    onClick={() => {
+                        setSearching(!searching);
+                        setLoggingIn(false);
+                    }}
+                >
+                    <SearchTwoToneIcon />
+                </IconButton>
+            )}
+            {loggingIn ? (
+                <Login />
+            ) : (
+                <Box>
+                    <Button
+                        onClick={() => {
+                            setSearching(false);
+                            setLoggingIn(!loggingIn);
+                        }}
+                    >
+                        LOGIN
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            setSearching(false);
+                            setLoggingIn(false);
+                            history.push('/register');
+                        }}
+                    >
+                        Sign Up
+                    </Button>
+                </Box>
+            )}
         </Box>
     );
 }
