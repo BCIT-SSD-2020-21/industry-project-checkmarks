daee74f76f20cc60aa6a7c1a5c9cba79a71bc0ad
veeepi <vpreduna90@gmail.com>
2021-04-23 23:32:05 -0700

    revise colorThemes; #42

--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -1,36 +1,44 @@
 const colors = {
-    lightRed: 'EE949B',
-    mediumRed: 'DF3440',
-    darkRed: 'A51A24',
+    lightRed: '#EE949B',
+    mediumRed: '#DF3440',
+    darkRed: '#A51A24',
 
-    lightBlue: '98ABD1',
-    mediumBlue: '4D6CAD',
-    darkBlue: '384F7E',
+    lightBlue: '#98ABD1',
+    mediumBlue: '#4D6CAD',
+    darkBlue: '#384F7E',
 
-    activeBlue: '12578B',
-    teal: '128B83',
+    activeBlue: '#12578B',
+    teal: '#128B83',
 
-    white: 'FFFFFF',
-    lightGray: '969696',
-    darkGray: '434343',
-    black: '000000',
+    white: '#FFFFFF',
+    lightestGray: '#ececec',
+    lightGray: '#969696',
+    darkGray: '#434343',
+    black: '#000000',
 };
 
-export const standardTheme = {
+export const checkmarksTheme = {
     bgPrimary: colors.white, // applied to Root Background
     bgSecondary: colors.lightRed, // light red (background for About section)
     // bgContainer: colors.white, // applied to Container Background
 
     textLabel: colors.darkGray, // applied to Text Label
     textValue: colors.black,
-    textPlaceholder: colors.lightGray,
+
+    inputLabel: colors.darkRed,
+    inputIcon: colors.darkRed,
+    inputBackground: colors.lightestGray,
+    inputPlaceholder: colors.lightGray,
+
     textActive: colors.activeBlue,
 
     borderPrimary: colors.mediumRed,
     borderSecondary: colors.darkRed,
 
+    buttonTextPrimary: colors.white,
     buttonPrimary: colors.mediumRed,
-    buttonSecondary: colors.lightRed,
+    buttonTextSecondary: colors.darkRed,
+    buttonSecondary: colors.white,
 
     bgGradientLight: colors.lightRed, // applied to Light side of linear gradient
     bgGradientDark: colors.darkRed, // applied to Dark side of linear gradient
