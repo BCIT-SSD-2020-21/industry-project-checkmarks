e671e3cf5b94817828f0d648c11d0a394c9f8578
fatmabadri <fbadri@my.bcit.ca>
2021-04-28 11:40:59 -0700

    create the elements of about page

--- src/components/LandingPage/About.js
+++ src/components/LandingPage/About.js
@@ -1,5 +1,88 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
-export default function About() {
-    return <div>About</div>;
-}
+const styles = (theme) => ({
+    root: {
+      display: 'flex',
+      backgroundColor: theme.palette.secondary.light,
+      overflow: 'hidden',
+    },
+    container: {
+      marginTop: theme.spacing(10),
+      marginBottom: theme.spacing(15),
+      position: 'relative',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    item: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: theme.spacing(0, 5),
+    },
+    title: {
+      marginBottom: theme.spacing(14),
+    },
+    number: {
+      fontSize: 24,
+      fontFamily: theme.typography.fontFamily,
+      color: theme.palette.secondary.main,
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    image: {
+      height: 100,
+      marginTop: theme.spacing(4),
+      marginBottom: theme.spacing(4),
+    },
+    curvyLines: {
+      pointerEvents: 'none',
+      position: 'absolute',
+      top: -180,
+      opacity: 0.7,
+    }
+  });
+  function About(props) {
+    const { classes } = props;
+  
+    return (
+      <section className={classes.root}>
+        <Container className={classes.container}>
+        <Typography variant="h4" marked="center" className={classes.title} component="h2">
+            About Us
+          </Typography>
+          <div>
+            <Grid container spacing={5}>
+              <Grid item xs={12} md={6}>
+                <div className={classes.item}>
+                  <Typography variant="h5" align="center">
+                  {'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.'}
+                  {''}
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <div className={classes.item}>
+                  <img
+                    src="../../images/CheckmarksLogo2.png"
+                    alt="image-about-us"
+                    className={classes.image}
+                  />
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </section>
+    );
+  }
+  
+  About.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(About);
