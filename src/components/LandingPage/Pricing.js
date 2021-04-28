d6d9908ad0d4f0af48f8decb48187f2fbbca2486
fatmabadri <fbadri@my.bcit.ca>
2021-04-28 12:05:34 -0700

    set the pricing tables

--- src/components/LandingPage/Pricing.js
+++ src/components/LandingPage/Pricing.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-export default function Pricing() {
-    return <div>Pricing</div>;
-}
+
+function Pricing(props) {
+    const { classes } = props;
+  
+    return (
+      <section className={classes.root}>
+        <Container className={classes.container}>
+          <Typography variant="h4" marked="center" className={classes.title} component="h2">
+            Pricing
+          </Typography>
+          <div>
+            <Grid container spacing={5}>
+              <Grid item xs={12} md={4}>
+              </Grid>
+              <Grid item xs={12} md={4}>
+              </Grid>
+              <Grid item xs={12} md={4}>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </section>
+    );
+  }
+  
+  Pricing.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(Pricing);
