1ae93a3ed9c09cd52bfa5e8f7cca541b8f43184b
fatmabadri <fbadri@my.bcit.ca>
2021-04-28 12:34:04 -0700

    update pricing table

--- src/components/LandingPage/Pricing.js
+++ src/components/LandingPage/Pricing.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
+const styles = (theme) => ({
+    root: {
+        display: 'flex',
+        backgroundColor: theme.palette.secondary.light,
+        overflow: 'hidden',
+      },
+      container: {
+        marginTop: theme.spacing(10),
+        marginBottom: theme.spacing(15),
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      item: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: theme.spacing(0, 5),
+      },
+      title: {
+        marginBottom: theme.spacing(14),
+      },
+      number: {
+        fontSize: 24,
+        fontFamily: theme.typography.fontFamily,
+        color: theme.palette.secondary.main,
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+    });
 
 function Pricing(props) {
     const { classes } = props;
@@ -13,10 +48,28 @@ function Pricing(props) {
           <div>
             <Grid container spacing={5}>
               <Grid item xs={12} md={4}>
+              <div className={classes.item}>
+                <div className={classes.number}>Basic Licence</div>
+                <Typography variant="h5" align="center">
+                Lorem ipsum dolor sit amet
+                </Typography>
+              </div>
               </Grid>
               <Grid item xs={12} md={4}>
+              <div className={classes.item}>
+                <div className={classes.number}>Standart Licence</div>
+                <Typography variant="h5" align="center">
+                Lorem ipsum dolor sit amet
+                </Typography>
+              </div>
               </Grid>
               <Grid item xs={12} md={4}>
+              <div className={classes.item}>
+                <div className={classes.number}>Premium Licence</div>
+                <Typography variant="h5" align="center">
+                Lorem ipsum dolor sit amet
+                </Typography>
+              </div>
               </Grid>
             </Grid>
           </div>
