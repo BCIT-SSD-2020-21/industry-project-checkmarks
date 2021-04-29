69b7049428e14d2979c8d8a6eb73c014fe083407
fatmabadri <fbadri@my.bcit.ca>
2021-04-29 11:27:49 -0700

    set the layout of the about page

--- src/components/LandingPage/About.js
+++ src/components/LandingPage/About.js
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-
 const useStyles = makeStyles((theme) => ({
   '@global': {
     ul: {
@@ -21,7 +20,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Pricing() {
+
+export default function About() {
   const classes = useStyles();
 
   return (
@@ -36,6 +36,29 @@ export default function Pricing() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit
           </Typography>
         </Container>  
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
     </React.Fragment>
   );
 }
+
