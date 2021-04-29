7c227405ef6fec6fc0581bbf0677cf1b76888cc1
veeepi <vpreduna90@gmail.com>
2021-04-28 20:42:41 -0700

    +func: convert tmType[int] to description[str];#89

--- src/services/checkmarks.js
+++ src/services/checkmarks.js
@@ -2,12 +2,41 @@ require('dotenv').config();
 const BASE = process.env.REACT_APP_BASE_URL + 'api/';
 const BASE_URL = BASE + 'trademark/';
 
+const tmTypeDescription = (tmTypeArray) => {
+    // (tmType: tmTypDescription)
+    var TRADEMARK_TYPES = {
+        1: 'Word',
+        2: 'Design',
+        3: 'Colour',
+        4: 'Three Dimensional',
+        5: 'Position',
+        6: 'Hologram',
+        7: 'Motion',
+        8: 'Sound',
+        9: 'Taste',
+        10: 'Scent',
+        11: 'Texture',
+        14: 'A Mode of Packaging Goods',
+        17: 'Standard Characters',
+    };
+
+    return tmTypeArray.reduce((tmDescriptions, thisTmType) => {
+        var thisTmTypeDescription = TRADEMARK_TYPES[thisTmType];
+        if (tmDescriptions.indexOf(thisTmTypeDescription) === -1) {
+            tmDescriptions.push(thisTmTypeDescription);
+        }
+
+        return tmDescriptions;
+    }, []);
+};
+
 export const formatSearchResults = (data) => {
     const formattedData = [];
-    data.forEach((item) => {
+    data?.forEach((item) => {
         // FORMAT DATE HERE
         let formattedItem = {
             ...item,
+            tmTypeDescriptions: tmTypeDescription(item.tmType),
             fileDateFormatted: item.fileDate.substring(0, 10),
         };
         formattedData.push(formattedItem);
@@ -22,5 +51,5 @@ export const searchTrademarks = async (term, password) => {
     const response = await fetch(url)
         .then((response) => response.json())
         .catch((error) => console.log('Error: ', error));
-    return formatSearchResults(response.data);
+    return formatSearchResults(response?.data);
 };
