8b098ee2d6374664f9a36474371e469fca0258ed
fatmabadri <fbadri@my.bcit.ca>
2021-04-28 21:30:27 -0700

    style card info

--- src/components/LandingPage/Pricing.js
+++ src/components/LandingPage/Pricing.js
@@ -15,6 +15,16 @@ const useStyles = makeStyles((theme) => ({
   heroContent: {
     padding: theme.spacing(8, 0, 6),
   },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 export default function Pricing() {
