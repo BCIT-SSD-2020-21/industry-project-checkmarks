03294bbc9bdc41cc077b9d92fb828ee8cafe4df2
veeepi <vpreduna90@gmail.com>
2021-05-01 02:07:40 -0700

    LandingPage, logo fade on search; resonposive;#104

--- src/layouts/Landing.js
+++ src/layouts/Landing.js
@@ -22,72 +22,36 @@ export default function Landing() {
     const classes = useStyles();
     const history = useHistory();
 
-    const [authenticated, setAuthenticated] = useState(false);
-    const [loggingIn, setLoggingIn] = useState(false);
-    const [searching, setSearching] = useState(true);
+    const [searching, setSearching] = useState(false);
 
     return (
         <Box className={classes.root}>
-            <MenuAppBar
-                authenticated={authenticated}
-                loggingIn={loggingIn}
-                setLoggingIn={setLoggingIn}
-                setSearching={setSearching}
-            />
-            {/* <Fade in={true} timeout={6000}> */}
+            <MenuAppBar setSearching={setSearching} />
             <Box className={classes.section}>
-                {/* <Fade in={true} exit={true} timeout={1000}> */}
-                {authenticated ? <UserDashboard /> : <HeaderBanner />}
-                {/* </Fade> */}
+                <HeaderBanner searching={searching} />
 
                 <Box className={classes.actions}>
                     <Box className={classes.search}>
-                        {searching ? (
-                            <TrademarkSearch />
-                        ) : (
-                            <IconButton
+                        {/* {infoDisplayToggle ? ( // idea */}
+                        <TrademarkSearch
+                            searching={searching}
+                            setSearching={setSearching}
+                        />
+                        {/* ) : ( */}
+                        {/* <IconButton
                                 className={classes.buttonSearch}
                                 onClick={() => {
                                     setSearching(!searching);
-                                    setLoggingIn(false);
                                 }}
                             >
                                 <SearchTwoToneIcon />
                             </IconButton>
-                        )}
-                    </Box>
-                    {/* {loggingIn ? (
-                            <Login />
-                        ) : (
-                            <Box className={classes.buttons}>
-                                <Button
-                                    className={classes.buttonRegister}
-                                    onClick={() => {
-                                        setSearching(false);
-                                        setLoggingIn(false);
-                                        history.push('/register');
-                                    }}
-                                    boxShadow={2}
-                                >
-                                    Register
-                                </Button>
-                                <Button
-                                    className={classes.buttonLogin}
-                                    onClick={() => {
-                                        setSearching(false);
-                                        setLoggingIn(!loggingIn);
-                                    }}
-                                    boxShadow={2}
-                                >
-                                    Login
-                                </Button>
-                            </Box>
                         )} */}
+                    </Box>
                     <Button
                         className={classes.buttonStart}
                         onClick={() => {
                             setSearching(false);
-                            setLoggingIn(false);
                             history.push('/application');
                         }}
                     >
@@ -96,7 +60,6 @@ export default function Landing() {
                     </Button>
                 </Box>
             </Box>
-            {/* </Fade> */}
             <About />
             <Pricing />
             <Footer />
@@ -127,10 +90,13 @@ const useStyles = makeStyles(() => ({
     actions: {
         // border: '1px solid black',
         // animation: '$shiftUp-actions 1s',
+        // backgroundColor: 'red',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
-        height: (window.innerHeight * 2) / 3,
+        height: (window.innerHeight * 3) / 4,
+        ['@media (min-width:768px)']: { height: (window.innerHeight * 2) / 3 },
+        ['@media (min-width:1280px)']: { height: (window.innerHeight * 2) / 3 },
         width: '90%',
     },
     search: {
@@ -162,44 +128,15 @@ const useStyles = makeStyles(() => ({
         borderRadius: '25px',
         color: checkmarksTheme.buttonTextPrimary,
         fontWeight: 'bold',
-        margin: 'auto',
+        margin: '10% auto 15% auto',
+        ['@media (min-width:768px)']: { margin: '10% auto 15%  auto' },
+        ['@media (min-width:1280px)']: { margin: '10% auto 12% auto' },
         opacity: 0.7,
         padding: '7px 0',
         minWidth: '300px',
         width: '50%',
         maxWidth: '320px',
     },
-    // buttonLogin: {
-    //     backgroundColor: checkmarksTheme.buttonSecondary,
-    //     '&:hover': {
-    //         color: checkmarksTheme.buttonPrimary,
-    //         background: checkmarksTheme.hoverLight,
-    //     },
-    //     border: `0.6px solid ${checkmarksTheme.buttonTextSecondary}`,
-    //     borderRadius: '25px',
-    //     color: checkmarksTheme.buttonTextSecondary,
-    //     fontWeight: 'bold',
-    //     opacity: 0.7,
-    //     padding: '5px 0',
-    //     width: '40%',
-    //     maxWidth: '120px',
-    // },
-    // buttonRegister: {
-    //     backgroundColor: checkmarksTheme.buttonPrimary,
-    //     '&:hover': {
-    //         color: checkmarksTheme.buttonPrimary,
-    //         background: checkmarksTheme.hoverSoft,
-    //     },
-    //     border: `0.6px solid ${checkmarksTheme.buttonTextSecondary}`,
-    //     borderRadius: '25px',
-    //     color: checkmarksTheme.buttonTextPrimary,
-    //     fontWeight: 'bold',
-    //     marginRight: '35px',
-    //     opacity: 0.7,
-    //     padding: '5px 0',
-    //     width: '55%',
-    //     maxWidth: '200px',
-    // },
     buttonSearch: {
         backgroundColor: checkmarksTheme.buttonSecondary,
         '&:hover': {
