ffcef30b55d5abb717fe4f3040d02721e25e81bf
veeepi <vpreduna90@gmail.com>
2021-05-02 22:12:25 -0700

    implement basic darkMode toggle; #58

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Paper, Switch as ClickSwitch } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Landing from './layouts/Landing';
 import TrademarkApplication from './layouts/TrademarkApplication';
 
 export default function App() {
+    const [darkMode, setDarkMode] = useState(false);
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: 'dark',
+        },
+    });
+    const lightTheme = createMuiTheme({
+        palette: {
+            type: 'light',
+        },
+    });
+
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Landing />
-                </Route>
-                <Route path="/application">
-                    <TrademarkApplication />
-                </Route>
-            </Switch>
-        </Router>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+            <Paper style={{ width: window.innerWidth }}>
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <Landing />
+                        </Route>
+                        <Route path="/application">
+                            <TrademarkApplication />
+                        </Route>
+                    </Switch>
+                </Router>
+                <ClickSwitch
+                    checked={darkMode}
+                    onChange={() => setDarkMode(!darkMode)}
+                />
+            </Paper>
+        </ThemeProvider>
     );
 }
