8532e2d8e37486d97b948d9155b0905d6d79bf2b
Kalvin Tang <ktang82@my.bcit.ca>
2021-05-05 00:50:42 -0700

    update to functional components #110

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,107 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from '@material-ui/core/Button';
 import React from 'react';
 
 const FileUpload = () => {
-    const [fileUpload, setFileUpload] = useState({
-        fileName: '',
-        uploadedFileName: '',
-        errorMessage: '',
-    });
+    const [fileName, setFileName] = useState('');
+    const [uploadedFileName, setUploadedFileName] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const fileInput = useRef(null);
 
-    // constructor(props) {
-    //     super(props);
-    //     this.handleSubmit = this.handleSubmit.bind(this);
-    //     this.handleUploadImage = this.handleUploadImage.bind(this);
-    //     this.fileInput = React.createRef();
+    useEffect(() => {
+        if (fileName) {
+            setUploadedFileName(fileName);
+        } else {
+            setFileName(fileName);
+        }
+    }, []);
 
-    //     this.state = {
-    //         fileName: '',
-    //         uploadedFileName: '',
-    //         errorMessage: '',
-    //     };
-    // }
-
-    // use effect
-
-    // componentDidMount() {
-    //     if (this.state.fileName) {
-    //         this.setState({
-    //             uploadedFileName: this.props.values.values.fileName,
-    //         });
-    //     } else {
-    //         this.setState({
-    //             fileName: this.props.values.values.fileName,
-    //         });
-    //     }
-    // }
     // Checks the file size and sets the image url preview.
-
-    // handleUploadImage(event) {
-    //     if (this.checkFileSize(event)) {
-    //         this.setState({
-    //             errorMessage: '',
-    //         });
-    //         this.props.oldState.file = URL.createObjectURL(
-    //             event.target.files[0]
-    //         );
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    const handleUploadImage = async (e) => {
-        e.preventDefault();
-
-        checkFileSize = (event) => {
-            let files = event.target.files[0];
-            let size = 2000000; //2Mb File Limit
-            let err = '';
-            if (files == null) {
-                return false;
-            }
-            if (files.size > size) {
-                err +=
-                    files.name + ' is too large, please pick a smaller file\n';
-            }
-            if (err !== '') {
-                event.target.value = null;
-                this.setState({ errorMessage: err });
-                return false;
-            }
+    const handleUploadImage = (event) => {
+        if (checkFileSize(event)) {
+            setErrorMessage(errorMessage);
+            // this.props.oldState.file = URL.createObjectURL(
+            //     event.target.files[0]
+            // );
             return true;
-        };
+        }
+        return false;
     };
 
+    const checkFileSize = (event) => {
+        let files = event.target.files[0];
+        let size = 2000000; //2Mb File Limit
+        let err = '';
+        if (files == null) {
+            return false;
+        }
+        if (files.size > size) {
+            err += files.name + ' is too large, please pick a smaller file\n';
+        }
+        if (err !== '') {
+            event.target.value = null;
+            setErrorMessage(err);
+            return false;
+        }
+        return true;
+    };
     // This handler is activated when the button is clicked. After calling handleUploadImage() it posts the image to the backend database for temporary storage.
-    // async handleSubmit(event) {
-    //     event.preventDefault();
-
-    //     if (this.handleUploadImage(event)) {
-    //         const formData = new FormData();
-
-    //         formData.append('FileToUpload', this.fileInput.current.files[0]);
-
-    //         const options = {
-    //             method: 'POST',
-    //             body: formData,
-    //         };
-
-    //         const response = await fetch(
-    //             this.props.values.values.checkmarksApiUrl + 'files',
-    //             options
-    //         )
-    //             .then((res) => {
-    //                 return res.json();
-    //             })
-    //             .then((data) => {
-    //                 this.props.handler('fileName', data.filename);
-
-    //                 this.setState({
-    //                     fileName: data.filename,
-    //                 });
-    //             });
-    //     }
-    //}
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (handleUploadImage(event)) {
+            const formData = new FormData();
+            formData.append('FileToUpload', fileInput.current.files[0]);
+            const options = {
+                method: 'POST',
+                body: formData,
+            };
+            const response = await fetch(
+                'https://localhost:5001/api/files',
+                options
+            )
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    // this.props.handler('fileName', data.filename);
+                    setFileName(data.fileName);
+                });
+        }
+    };
     return (
         <div>
             <form>
@@ -111,29 +77,24 @@ const FileUpload = () => {
                         (single image file under 2MB):
                     </span>
                 </p>
-
                 <Button
                     variant="contained"
                     component="label"
                     color="primary"
-                    onChange={this.handleSubmit}
+                    onChange={handleSubmit}
                 >
                     Choose File
                     <input
                         encType="multipart/form-data"
                         accept="image/* "
                         type="file"
-                        ref={this.fileInput}
+                        ref={fileInput}
                         style={{ display: 'none' }}
                     />
                 </Button>
-                {this.state.fileName && (
-                    <p>Currently using {this.state.fileName}</p>
-                )}
-                {this.state.errorMessage && (
-                    <p className="errorMessage">{this.state.errorMessage}</p>
-                )}
-                <p>{this.state.uploadedFileName}</p>
+                {fileName && <p>Currently using {fileName}</p>}
+                {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+                <p>{uploadedFileName}</p>
                 <p className="finePrint">
                     If you wish to use more than one file, or a larger one,
                     please follow up with the confirmation email sent at the end
