476c00697c8acc00932e628753205b2bea0fa0de
fatmabadri <fbadri@my.bcit.ca>
2021-05-06 12:57:32 -0700

    create faq.js component

---
+++ src/components/LandingPage/Faq.js
@@ -0,0 +1,20 @@
+import React from 'react';
+   
+export default function Faq() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* Hero unit */}
+        <Container maxWidth="sm" component="main" className={classes.heroContent}>
+          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            F.A.Q
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" component="p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </Typography>
+        </Container>  
+    </React.Fragment>
+  );
+}
