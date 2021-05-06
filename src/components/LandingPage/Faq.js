e4123abd74caa7942019301e3376775315b793cd
fatmabadri <fbadri@my.bcit.ca>
2021-05-06 13:42:04 -0700

    import accordion mui components

--- src/components/LandingPage/Faq.js
+++ src/components/LandingPage/Faq.js
@@ -3,6 +3,10 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
