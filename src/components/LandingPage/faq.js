59f240c885bdc248ac7e2c7f073890e4217fce8b
karenm08 <wmok14@my.bcit.ca>
2021-05-11 12:49:07 -0700

    prettier reformating - faq #101

--- src/components/LandingPage/faq.js
+++ src/components/LandingPage/faq.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
@@ -8,197 +8,140 @@ import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 import { withStyles } from '@material-ui/core/styles';
 
+
 const useStyles = makeStyles((theme) => ({
     root: {
-        width: '70%',
+      width: '70%',
     },
     heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
     },
     '@global': {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: 'none',
-        },
+      ul: {
+        margin: 0,
+        padding: 0,
+        listStyle: 'none',
+      },
     },
     heroContent: {
-        padding: theme.spacing(8, 0, 6),
+      padding: theme.spacing(8, 0, 6),
     },
-}));
+  }));
 
-const Accordion = withStyles({
+  const Accordion = withStyles({
     root: {
-        border: '1px solid rgba(0, 0, 0, .125)',
-        boxShadow: 'none',
-        '&:not(:last-child)': {
-            borderBottom: 0,
-        },
-        '&:before': {
-            display: 'none',
-        },
-        '&$expanded': {
-            margin: 'auto',
-        },
+      border: '1px solid rgba(0, 0, 0, .125)',
+      boxShadow: 'none',
+      '&:not(:last-child)': {
+        borderBottom: 0,
+      },
+      '&:before': {
+        display: 'none',
+      },
+      '&$expanded': {
+        margin: 'auto',
+      },
     },
     expanded: {},
-})(MuiAccordion);
+  })(MuiAccordion);
 
-const AccordionSummary = withStyles({
+  const AccordionSummary = withStyles({
     root: {
-        backgroundColor: 'rgba(165, 26, 36, 0.80)',
-        borderBottom: '1px solid rgba(0, 0, 0, .125)',
-        marginBottom: -1,
+      backgroundColor: 'rgba(165, 26, 36, 0.80)',
+      borderBottom: '1px solid rgba(0, 0, 0, .125)',
+      marginBottom: -1,
+      minHeight: 56,
+      '&$expanded': {
         minHeight: 56,
-        '&$expanded': {
-            minHeight: 56,
-        },
+      },
     },
     content: {
-        '&$expanded': {
-            margin: '12px 0',
-        },
+      '&$expanded': {
+        margin: '12px 0',
+      },
     },
     expanded: {},
-})(MuiAccordionSummary);
+  })(MuiAccordionSummary);
 
-const AccordionDetails = withStyles((theme) => ({
+  const AccordionDetails = withStyles((theme) => ({
     root: {
-        backgroundColor: 'rgba(255, 0, 0, .25)',
-        padding: theme.spacing(2),
+      backgroundColor: 'rgba(255, 0, 0, .25)',
+      padding: theme.spacing(2),
     },
-}))(MuiAccordionDetails);
-
+  }))(MuiAccordionDetails);
+  
 export default function Faq() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [expanded, setExpanded] = useState('panel1');
+  const [expanded, setExpanded] = useState('panel1');
 
-    const handleChange = (panel) => (event, newExpanded) => {
-        setExpanded(newExpanded ? panel : false);
-    };
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
 
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            {/* Hero unit */}
-            <Container
-                maxWidth="sm"
-                component="main"
-                className={classes.heroContent}
-            >
-                <Typography
-                    component="h1"
-                    variant="h2"
-                    align="center"
-                    color="textPrimary"
-                    gutterBottom
-                >
-                    F.A.Q
-                </Typography>
-                <Typography
-                    variant="h5"
-                    align="center"
-                    color="textSecondary"
-                    component="p"
-                >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </Typography>
-            </Container>
-            <Container>
-                {/*  /// PANEL 1 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel1'}
-                    onChange={handleChange('panel1')}
-                >
-                    <AccordionSummary
-                        aria-controls="panel1d-content"
-                        id="panel1d-header"
-                    >
-                        <Typography>What is a trademark?</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Suspendisse malesuada lacus ex, sit amet
-                            blandit leo lobortis eget. Lorem ipsum dolor sit
-                            amet, consectetur adipiscing elit.
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-                {/*  /// PANEL 2 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel2'}
-                    onChange={handleChange('panel2')}
-                >
-                    <AccordionSummary
-                        aria-controls="panel2d-content"
-                        id="panel2d-header"
-                    >
-                        <Typography>
-                            What is the difference between a trade name and a
-                            trademark?
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Suspendisse malesuada lacus ex, sit amet
-                            blandit leo lobortis eget. Lorem ipsum dolor sit
-                            amet, consectetur adipiscing elit.
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-                {/*  /// PANEL 3 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel3'}
-                    onChange={handleChange('panel3')}
-                >
-                    <AccordionSummary
-                        aria-controls="panel3d-content"
-                        id="panel3d-header"
-                    >
-                        <Typography>
-                            Why should I care about search and review services?
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Suspendisse malesuada lacus ex, sit amet
-                            blandit leo lobortis eget. Lorem ipsum dolor sit
-                            amet, consectetur adipiscing elit.
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-                {/*  /// PANEL 4 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel4'}
-                    onChange={handleChange('panel4')}
-                >
-                    <AccordionSummary
-                        aria-controls="panel4d-content"
-                        id="panel4d-header"
-                    >
-                        <Typography>
-                            How do trademark lawyers or IP agents help?
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Suspendisse malesuada lacus ex, sit amet
-                            blandit leo lobortis eget. Lorem ipsum dolor sit
-                            amet, consectetur adipiscing elit.
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            </Container>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* Hero unit */}
+        <Container maxWidth="sm" component="main" className={classes.heroContent}>
+          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            F.A.Q
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" component="p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </Typography>
+        </Container>     
+        <Container> 
+            {/*  /// PANEL 1 ////  */}
+            <Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                    <Typography>What is a trademark?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit. 
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            {/*  /// PANEL 2 ////  */}
+            <Accordion square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+                    <Typography>What is the difference between a trade name and a trademark?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit. 
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            {/*  /// PANEL 3 ////  */}
+            <Accordion square expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+                    <Typography>Why should I care about search and review services?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit. 
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            {/*  /// PANEL 4 ////  */}
+            <Accordion square expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
+                    <Typography>How do trademark lawyers or IP agents help?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit. 
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Container> 
+    </React.Fragment>
+  );
 }
