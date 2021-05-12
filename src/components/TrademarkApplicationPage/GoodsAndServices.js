7504dac650189ef59d021dee42f6b20d74ccb206
veeepi <vpreduna90@gmail.com>
2021-05-12 07:44:58 -0700

    fix bug classes removed if no terms; #164

--- src/components/TrademarkApplicationPage/GoodsAndServices.js
+++ src/components/TrademarkApplicationPage/GoodsAndServices.js
@@ -39,8 +39,8 @@ export default function GoodsAndServices({
 
     // INPUT statevar
     const [searchTerm, setSearchTerm] = useState(''); // user's search
+    const [searchClassFilter, setSearchClassFilter] = useState(''); // user to filter results by class name
     const [open, setOpen] = useState(false); // dialog box showing when no terms selected
-
     // INITIALIZE From 'info' Statevar
     const [selectedTerms, setSelectedTerms] = useState(info.termsSelected); // rendered on Selected Terms summary
     const [selectedClasses, setSelectedClasses] = useState(
@@ -57,7 +57,7 @@ export default function GoodsAndServices({
         setTermBeingToggledNumber(null);
     }, [termBeingToggledNumber]);
 
-    // GET TERMS AFTER TEXT INPUT
+    // GET TERMS AFTER TEXT INPUT (with Delay)
     const [termSearchResults, setTermSearchResults] = useState([]);
     const { current: searchInstance } = useRef({});
     useEffect(() => {
@@ -78,7 +78,7 @@ export default function GoodsAndServices({
         }
     }, [searchTerm]);
 
-    // GET TERM DATA (on Search)
+    // RESULTS FROM PREVIOUS GET, POPULATED TO TERM TABLE ARRAY, which is Rendered Rendered
     const [termTableData, setTermTableData] = useState([]); // DATA Rendering on Table (Displayed)
     useEffect(() => {
         renderTerms();
@@ -183,7 +183,9 @@ export default function GoodsAndServices({
         }
     };
 
-    // filter selectedTerms, get list of selected classes (no duplicates)
+    console.log('info: ', info);
+
+    // upon selectedTerms update, run logic to change selectedClasses (no duplicates) and totalAmount
     useEffect(() => {
         const classesSelected = [];
         if (selectedTerms.length > 0) {
@@ -210,10 +212,12 @@ export default function GoodsAndServices({
             } else if (classesSelected.length === 0) {
                 setTotalAmount(0);
             }
+        } else {
+            setSelectedClasses([]);
+            setTotalAmount(0);
         }
-        if (selectedTerms?.length > 0) {
-            setInfo({ ...info, termsSelected: selectedTerms });
-        }
+
+        setInfo({ ...info, termsSelected: selectedTerms });
     }, [selectedTerms]);
 
     // UPDATE PARENT Statevar 'info'
@@ -227,6 +231,7 @@ export default function GoodsAndServices({
         }
     }, [selectedClasses]);
 
+    // Functions to handle Next/Previous Steps
     const previousStep = () => {
         setCurrentStep(currentStep - 1); // assign currentStep to next step
         navigation.previous();
@@ -402,11 +407,11 @@ export default function GoodsAndServices({
                                                                 <IconButton
                                                                     color="secondary"
                                                                     variant="contained"
-                                                                    onClick={() =>
+                                                                    onClick={() => {
                                                                         removeTerm(
                                                                             term
-                                                                        )
-                                                                    }
+                                                                        );
+                                                                    }}
                                                                 >
                                                                     <DeleteForeverTwoToneIcon />
                                                                 </IconButton>
@@ -610,229 +615,3 @@ const useStyles = makeStyles((theme) => ({
         },
     },
 }));
-
-// From Above: (Ref# 12345678)
-// constructor(props) {
-//     super(props);
-//     this.clickContinue = this.clickContinue.bind(this);
-
-//     this.state = {
-//         terms: [],
-//         classShortNames: [],
-//         selectedClasses: [],
-//         selectedTerms: [],
-//         searchTerm: '',
-//         searchError: '',
-//         open: false,
-//     };
-// }
-
-// componentDidMount() {
-//     if (this.props.values.classes.length > 0) {
-//         this.setState({
-//             selectedClasses: this.props.values.classes.slice(),
-//         });
-//     }
-//     if (this.props.values.terms.length > 0) {
-//         this.setState({
-//             selectedTerms: this.props.values.terms.slice(),
-//         });
-//     }
-//     this.fetchClassShortName();
-// }
-
-// const clickContinue = (e) => {
-//     if (selectedTerms.length == 0) {
-//       setOpen(true)
-//     } else {
-//         this.continue(e);
-//     }
-// };
-
-//const continue = (e) => {
-//     e.preventDefault();
-//     this.props.handler('classes', this.state.selectedClasses);
-//     this.props.handler('terms', this.state.selectedTerms);
-//     this.changeAmount();
-//     this.props.nextStep();
-// };
-
-// back = (e) => {
-//     e.preventDefault();
-//     this.props.prevStep();
-// };
-
-// changeAmount() {
-//     var newAmount = 150000;
-//     if (this.state.selectedClasses.length > 1) {
-//         newAmount += (this.state.selectedClasses.length - 1) * 100 * 100;
-//     }
-//     this.props.handler('amount', newAmount);
-// }
-
-// handleClose = () => {
-//     this.setState({ open: false });
-// };
-
-// //Checks for any speical characters in the search string. Prints error message if true.
-// validateSearchString() {
-//     let searchError = '';
-//     if (!this.state.searchTerm) {
-//         searchError = 'Please enter a word that describes your trademark';
-//     }
-//     if (this.state.searchTerm.includes('/')) {
-//         searchError = "Your search cannot include '/'";
-//     }
-//     if (
-//         this.state.searchTerm.includes('.') ||
-//         this.state.searchTerm.includes(';') ||
-//         this.state.searchTerm.includes('$') ||
-//         this.state.searchTerm.includes(',') ||
-//         this.state.searchTerm.includes(':') ||
-//         this.state.searchTerm.includes('?') ||
-//         this.state.searchTerm.includes('@') ||
-//         this.state.searchTerm.includes('#') ||
-//         this.state.searchTerm.includes('!') ||
-//         this.state.searchTerm.includes('&') ||
-//         this.state.searchTerm.includes('*') ||
-//         this.state.searchTerm.includes('(') ||
-//         this.state.searchTerm.includes("'") ||
-//         this.state.searchTerm.includes(')') ||
-//         this.state.searchTerm.includes('"') ||
-//         this.state.searchTerm.includes('}') ||
-//         this.state.searchTerm.includes('-') ||
-//         this.state.searchTerm.includes('{') ||
-//         this.state.searchTerm.includes('%')
-//     ) {
-//         searchError = 'Please do not include any special characters';
-//     }
-//     if (searchError) {
-//         this.setState({ searchError });
-//         return false;
-//     } else {
-//         this.setState({ searchError });
-//     }
-//     return true;
-// }
-
-// //Fetches the term information from the backend web api
-// getSearchTerms = () => {
-//     var searchString = this.state.searchTerm;
-//     if (this.validateSearchString()) {
-//         let url =
-//             this.props.values.checkmarksApiUrl +
-//             'cipo/GetTermDataByString/' +
-//             searchString;
-//         fetch(url)
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 this.setState({
-//                     terms: data.terms,
-//                 });
-//             });
-//     }
-// };
-
-// handleTextFieldChange = (event) => {
-//     this.setState({
-//         searchTerm: event.target.value,
-//     });
-// };
-
-// //Checks if the term has already been entered in the list
-// isDupTerm(termList, term) {
-//     if (termList.length == 0) {
-//         return false;
-//     } else {
-//         var i;
-//         var x;
-//         for (i = 0; i < termList.length; i++) {
-//             for (x = 0; x < termList[i].length; x++) {
-//                 if (termList[i][x] == term) {
-//                     return true;
-//                 }
-//             }
-//         }
-//     }
-//     return false;
-// }
-
-// //Checks for any duplicate classes. If true, returns its index, else returns -1
-// checkDupClass(classList, newClassNum) {
-//     var i;
-//     for (i = 0; i < classList.length; i++) {
-//         if (classList[i] == newClassNum) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-
-// async fetchClassShortName() {
-//     let tempShortNames = [];
-//     let url = this.props.values.checkmarksApiUrl + 'cipo/GetAllClasses/';
-//     await fetch(url)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             var i;
-//             for (i = 0; i < data.classes.length; i++) {
-//                 tempShortNames.push(data.classes[i].name);
-//             }
-//         });
-//     this.setState({
-//         classShortNames: tempShortNames,
-//     });
-// }
-
-// getClassShortName(index) {
-//     return this.state.classShortNames[index - 1];
-// }
-
-// //Adds the term and class into the selectedTerms and selectedClasses lists, while also checking for duplicates
-// handleAdd(data) {
-//     var tempClassList = this.state.selectedClasses.slice(); //Slice returns a copy of array rather than a reference
-//     var tempTermList = this.state.selectedTerms.slice();
-
-//     data.forEach((element) => {
-//         var classIndex = this.checkDupClass(
-//             tempClassList,
-//             element.termClass
-//         );
-//         if (classIndex == -1) {
-//             tempClassList.push(element.termClass);
-//             tempTermList.push(Array(element.termName));
-//         } else {
-//             if (!this.isDupTerm(tempTermList, element.termName)) {
-//                 tempTermList[classIndex].push(element.termName);
-//             }
-//         }
-//     });
-
-//     this.setState({
-//         selectedClasses: tempClassList,
-//         selectedTerms: tempTermList,
-//     });
-// }
-
-// //Removes the term from the list, and removes the class if there are no more terms left for that class.
-// handleRemove(classNumber, term) {
-//     var tempClassList = this.state.selectedClasses.slice();
-//     var tempTermList = this.state.selectedTerms.slice();
-//     var classIndex = this.state.selectedClasses.indexOf(classNumber);
-
-//     for (var i = 0; i < tempTermList[classIndex].length; i++) {
-//         if (tempTermList[classIndex][i].localeCompare(term) == 0) {
-//             tempTermList[classIndex].splice(i, 1);
-//         }
-//     }
-
-//     if (tempTermList[classIndex].length == 0) {
-//         tempTermList.splice(classIndex, 1);
-//         tempClassList.splice(classIndex, 1);
-//     }
-
-//     this.setState({
-//         selectedClasses: tempClassList,
-//         selectedTerms: tempTermList,
-//     });
-// }
