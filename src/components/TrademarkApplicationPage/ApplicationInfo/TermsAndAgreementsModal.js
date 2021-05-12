0421ed2c10564a2209109c27548dfaba632cbc60
Kalvin Tang <ktang82@my.bcit.ca>
2021-05-12 09:55:36 -0700

    #126 add make style for modal plus library imports

--- src/components/TrademarkApplicationPage/ApplicationInfo/TermsAndAgreementsModal.js
+++ src/components/TrademarkApplicationPage/ApplicationInfo/TermsAndAgreementsModal.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Modal, Backdrop, Fade } from '@material-ui/core';
 
 const TermsAndAgreementsModal = () => {
+    const classes = useStyles();
+
     return (
         <Modal
             aria-labelledby="transition-modal-title"
@@ -48,3 +52,17 @@ const TermsAndAgreementsModal = () => {
 };
 
 export default TermsAndAgreementsModal;
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
