80e81058152a4851111c2d5809101a98acffd513
fatmabadri <fbadri@my.bcit.ca>
2021-05-12 23:46:34 -0700

    add cta button

--- src/components/LandingPage/Cta.js
+++ src/components/LandingPage/Cta.js
@@ -5,6 +5,11 @@ function CallToAction(props) {
 
   return (
     <Container className={classes.root} component="section">
+        <Button className={classes.button}>
+            <Typography variant="h4" component="span">
+                Registering Your Trademark? Need Assistance?
+            </Typography>
+        </Button>
     </Container>
   );
 }
