d04b4e8a8af8ff289a31262b35dbf92b2292af48
veeepi <vpreduna90@gmail.com>
2021-05-13 07:44:06 -0700

    add content to import to Modal (WIP); #106

---
+++ src/services/content.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+export const TrademarkRulesCIPO = () => (
+    <div>
+        <h5>
+            Below are some rules of thumb regarding Trademarks as provided by
+            the Canadian Intellectual Property Office.{' '}
+        </h5>
+
+        <h3>Names and Surnames</h3>
+        <p>
+            A trademark may not be registered if it is nothing more than a name
+            or surname. An exception is if you can prove that your goods or
+            services have become well known under the name or surname so that
+            the word has acquired a second meaning in the public mind.
+        </p>
+        <hr />
+        <h3>Clearly descriptive marks</h3>
+        <p>
+            You may not register a trademark that clearly describes a
+            characteristic or quality of your goods or services.
+        </p>
+        <h3>Clearly descriptive marks</h3>
+        <p>
+            You may not register a trademark that clearly describes a
+            characteristic or quality of your goods or services.
+        </p>
+        <hr />
+        <h3>Deceptively misdescriptive marks</h3>
+        <p>
+            You may not register a trademark that describes the geographical
+            location where the goods or services come from. Allowing you to use
+            such place names as your trademark would mean you are the only one
+            who can use the geographical place name, and that would be unfair to
+            others who trade in that place. For example, you could not register
+            "Italy" for lasagna. Also, you may not register a word that misleads
+            the public into thinking that the goods or services come from a
+            certain place when they do not. For example, you could not register
+            "Paris Fashions" or "Denmark Furniture" as a trademark for goods or
+            services if they did not come from there.
+        </p>
+        <hr />
+        <h3>Words in other languages</h3>
+        <p>
+            You may not register trademarks that are the name, in any language,
+            of the goods or services associated with your trademark. For
+            example, you would not be able to register the word "gelato"
+            (Italian for "ice cream") in association with frozen confections;
+            "anorak" (Inuktitut for "parka") in association with outerwear; or
+            "wurst" (German for "sausage") in association with meat.
+        </p>
+        <hr />
+        <h3>Confusing with a registered or pending trademark</h3>
+        <p>
+            Beware of trademarks that are similar to another trademark that is
+            registered or is the subject of a previously-filed application. If
+            your trademark is confusingly similar to a registered trademark or a
+            pending trademark, it will be refused.{' '}
+        </p>
+        <hr />
+        <h3>
+            Trademarks that are identical to, or likely to be mistaken for,
+            prohibited marks
+        </h3>
+        <p>
+            You may not register a trademark that is identical or similar to
+            certain official marks unless you have the permission from the
+            organization that controls the mark.
+        </p>
+        <hr />
+    </div>
+);
