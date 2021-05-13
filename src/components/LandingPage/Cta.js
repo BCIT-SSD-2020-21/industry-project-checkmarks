ac30a8626c6ea28b60c68944639f538866e17072
fatmabadri <fbadri@my.bcit.ca>
2021-05-12 23:51:05 -0700

    style the container

--- src/components/LandingPage/Cta.js
+++ src/components/LandingPage/Cta.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 
 function CallToAction(props) {
+    const { classes } = props;
 
-  return (
-    <Container className={classes.root} component="section">
-        <Button className={classes.button}>
-            <Typography variant="h4" component="span">
-                Registering Your Trademark? Need Assistance?
+    return (
+        <Container className={classes.root} component="section">
+            <Button className={classes.button}>
+                <Typography variant="h4" component="span">
+                    Registering Your Trademark? Need Assistance?
+                </Typography>
+            </Button>
+            <Typography variant="subtitle1" className={classes.link}>
+                We are here to help you. Get in touch!
             </Typography>
-        </Button>
-        <Typography variant="subtitle1" className={classes.link}>
-            We are here to help you. Get in touch!
-        </Typography>
-    </Container>
-  );
+        </Container>
+    );
 }
