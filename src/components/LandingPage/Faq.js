08cfc95445018fb7a17c3d0a8104c82788e4534a
fatmabadri <fbadri@my.bcit.ca>
2021-05-13 17:05:07 -0700

    add fifth faq

--- src/components/LandingPage/Faq.js
+++ src/components/LandingPage/Faq.js
@@ -88,14 +88,14 @@ export default function Faq() {
                 maxWidth="sm"
                 component="main"
                 className={classes.heroContent}
-            >
+                >
                 <Typography
                     component="h1"
                     variant="h2"
                     align="center"
                     color="textPrimary"
                     gutterBottom
-                >
+                    >
                     F.A.Q
                 </Typography>
                 <Typography
@@ -103,77 +103,84 @@ export default function Faq() {
                     align="center"
                     color="textSecondary"
                     component="p"
-                >
+                    >
                     {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit */}
                 </Typography>
             </Container>
             <Container>
-                {/*  /// PANEL 1 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel1'}
-                    onChange={handleChange('panel1')}
+            {/*  /// PANEL 1 ////  */}
+            <Accordion
+                square
+                expanded={expanded === 'panel1'}
+                onChange={handleChange('panel1')}
                 >
-                    <AccordionSummary
-                        aria-controls="panel1d-content"
-                        id="panel1d-header"
+                <AccordionSummary
+                    aria-controls="panel1d-content"
+                    id="panel1d-header"
                     >
-                        <Typography>What is a trademark?</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                        A trademark is like a fingerprint for businesses. It is something that distinguishes your goods or services from others. <br /> 
-                        Trademarks include brands, names, slogans, logos, colours, sounds, textures, the way something is packaged, the layout of a store, etc
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-                {/*  /// PANEL 2 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel2'}
-                    onChange={handleChange('panel2')}
+                    <Typography>What is a trademark?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                    A trademark is like a fingerprint for businesses. It is something that distinguishes your goods or services from others. <br /> 
+                    Trademarks include brands, names, slogans, logos, colours, sounds, textures, the way something is packaged, the layout of a store, etc
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            {/*  /// PANEL 2 ////  */}
+            <Accordion
+                square
+                expanded={expanded === 'panel2'}
+                onChange={handleChange('panel2')}
                 >
-                    <AccordionSummary
-                        aria-controls="panel2d-content"
-                        id="panel2d-header"
+                <AccordionSummary
+                    aria-controls="panel2d-content"
+                    id="panel2d-header"
                     >
-                        <Typography>
-                        What is the difference between a trade name and a trademark?
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                        A trade name is the name of your business, which is sometimes the tradename is the trademark
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-                {/*  /// PANEL 3 ////  */}
-                <Accordion
-                    square
-                    expanded={expanded === 'panel3'}
-                    onChange={handleChange('panel3')}
+                    <Typography>
+                    What is the difference between a trade name and a trademark?
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                    A trade name is the name of your business, which is sometimes the tradename is the trademark
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            {/*  /// PANEL 3 ////  */}
+            <Accordion
+                square
+                expanded={expanded === 'panel3'}
+                onChange={handleChange('panel3')}
                 >
-                    <AccordionSummary
-                        aria-controls="panel3d-content"
-                        id="panel3d-header"
+                <AccordionSummary
+                    aria-controls="panel3d-content"
+                    id="panel3d-header"
+                     >
+                    <Typography>Should you register your trademark?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                    Generally, simply making and using a trademark gives you a trademark (called a <strong>“common law trademark”</strong>). This is free but relates to real goodwill enjoyed by the trademark and so it can be hard to enforce across Canada. If you only have goodwill in Vancouver, it will very hard to enforce your common law trademark rights in Halifax.  Registering a trademark with the Canadian Intellectual Property Office <strong>(CIPO)</strong> and there are several advantages to doing so:
+                    <br/>
+                    (a)	Registration of your trademark will give you Canada-wide rights to the exclusive use of that mark in your industry regardless of whether your trademark enjoys goodwill or reputation in any particular area of Canada;<br/>
+                    (b)	Registration allows you to take easier steps to prevent others from using an identical or confusingly similar trademark in relation to a similar business;<br/>
+                    (c)	CIPO will screen future trademark registration applications thereby offering your registered trademark some additional protection;<br/>
+                    (d)	Trademark registration will deter others from adopting a trademark similar to yours; and<br/>
+                    (e)	Trademark registration may prevent others from registering a trademark similar to yours, and then taking action against you for damages or to prevent you from using your established trademark.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            {/*  /// PANEL 4 ////  */}
+            <Accordion 
+                square 
+                expanded={expanded === 'panel4'} 
+                onChange={handleChange('panel4')}
+                >
+                <AccordionSummary 
+                    aria-controls="panel4d-content" 
+                    id="panel4d-header"
                     >
-                        <Typography>Should you register your trademark?</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                        Generally, simply making and using a trademark gives you a trademark (called a <strong>“common law trademark”</strong>). This is free but relates to real goodwill enjoyed by the trademark and so it can be hard to enforce across Canada. If you only have goodwill in Vancouver, it will very hard to enforce your common law trademark rights in Halifax.  Registering a trademark with the Canadian Intellectual Property Office <strong>(CIPO)</strong> and there are several advantages to doing so:
-                        <br/>
-                        (a)	Registration of your trademark will give you Canada-wide rights to the exclusive use of that mark in your industry regardless of whether your trademark enjoys goodwill or reputation in any particular area of Canada;<br/>
-                        (b)	Registration allows you to take easier steps to prevent others from using an identical or confusingly similar trademark in relation to a similar business;<br/>
-                        (c)	CIPO will screen future trademark registration applications thereby offering your registered trademark some additional protection;<br/>
-                        (d)	Trademark registration will deter others from adopting a trademark similar to yours; and<br/>
-                        (e)	Trademark registration may prevent others from registering a trademark similar to yours, and then taking action against you for damages or to prevent you from using your established trademark.
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-                {/*  /// PANEL 4 ////  */}
-                <Accordion square expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-                <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
                     <Typography>How long is the “trademarking” process?</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
@@ -184,22 +191,27 @@ export default function Faq() {
                 </AccordionDetails>
             </Accordion>
             {/*  /// PANEL 5 ////  */}
-            {/* <Accordion
-                    square
-                    expanded={expanded === 'panel1'}
-                    onChange={handleChange('panel1')}
+            <Accordion 
+                square expanded={expanded === 'panel5'} 
+                onChange={handleChange('panel5')}
                 >
-                    <AccordionSummary
-                        aria-controls="panel1d-content"
-                        id="panel1d-header"
+                <AccordionSummary 
+                    aria-controls="panel5d-content" 
+                    id="pane5d-header"
                     >
-                        <Typography></Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion> */}
+                    <Typography>What are the general steps in a trademark registration?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <strong>Search + Report.</strong> Do a detailed search of your trademark  - how likely is the application to succeed<br/>
+                        <strong>Draft + File.</strong>Draft and file an application for registration. This includes attaching the trademark to specific goods and services. <br/>
+                        <strong>Deal with Examiner.</strong> A CIPO examiner will look at the application for formal, technical, or legal objections. If none, then we move to the next step. If there is an objection,  then that will need to be replied to or otherwise dealt with to try to overcome their objections.<br/>
+                        <strong>Advertise. </strong> Once past the Examiner, the application is publicly advertised for 2 months. During this period any 3rd party can oppose your application. This is like mini-ligation. If this happens, it must be resolved before proceeding.<br/>
+                        <strong>Done. </strong> You'll get a registered trademark.<br/>
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
             </Container>
         </React.Fragment>
     );
