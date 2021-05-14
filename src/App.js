0d31b595b257b9b0697056f962680803a6a810e1
veeepi <vpreduna90@gmail.com>
2021-05-14 02:36:22 -0700

    move state variables to App.js; #208

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useStep } from 'react-hooks-helper';
 import { Paper } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import MenuAppBar from './components/AppBar';
 import Landing from './layouts/Landing';
 import TrademarkApplication from './layouts/TrademarkApplication';
+import { sumProgressValue, validateForm } from './utils/FormValidation';
 
 export default function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -30,13 +32,154 @@ export default function App() {
         // setProgressBarPosition(0)
     };
 
-    console.log('App, drawerOpen: ', drawerOpen);
+    // form information
+    const [info, setInfo] = useState({
+        //Application Information
+        individualOrOrganization: 'Individual',
+        firstName: '',
+        lastName: '',
+        organizationName: '',
+        email: '',
+        idDocumentUploaded: false,
+        userStreetAddress: '',
+        userCity: '',
+        userProvince: '',
+        userPostalCode: '',
+        userCountry: '',
+        agreedTermsOfService: false,
+        idName: '',
+
+        //Trademark Type
+        isText: false,
+        isLogo: false,
+        isOther: false,
+        otherTypes: [],
+        characterText: '',
+        fileName: '',
+        file: '',
+
+        // Goods and Services
+        classesSelected: [],
+        termsSelected: [],
+        selectedServiceName: 'DIY',
+        basePrice: 690,
+        amount: 0,
+
+        //International Information
+        filedInOtherCountry: null,
+        countryOfFiling: '',
+        fillingDate: '',
+        fillingNumber: '',
+
+        // Info Confirmed
+        infoConfirmed: false,
+
+        // Payment Information
+        paymentToken: '',
+        paymentCardholderName: '',
+        paymentCreditCardNumber: '',
+        paymentCardExpiryDate: '',
+        paymentCardCVV: '',
+        billingAddressSameAsUser: false,
+        billingStreetAddress: '',
+        billingPostalCode: '',
+        paymentConfirmaed: false,
+    });
+
+    // form validation and progress
+    const [validationProgress, setValidationProgress] = useState({
+        //Application Informarion - 1100 - step 1 end
+        individualOrOrganizationName: 0,
+        firstName: 0,
+        lastName: 0,
+        email: 0,
+        idDocumentUploaded: 0,
+        userStreetAddress: 0,
+        userCity: 0,
+        userProvince: 0,
+        userPostalCode: 0,
+        userCountry: 0,
+        agreedTermsOfService: 0,
+
+        //Trademark Type - 500 - step 2 end
+        trademarkTypeFormCompleted: 0,
+
+        // Goods and Services - 500 - step 3 end
+        amountNotZero: 0,
+
+        //International Information - 300 - step 4 end
+        internationalFilingInfo: 0,
+
+        //Info Confirmed - 200 - step 5 end
+        infoConfirmed: 0,
+
+        // Payment Information - 300 - step 6 end
+        paymentConfirmed: 0,
+    });
+    useEffect(() => {
+        // builds the object carrying validation values assoiated with each step
+        validateForm(info, setValidationProgress);
+    }, [info]);
+    const [progressValue, setProgressValue] = useState(0);
+    useEffect(() => {
+        // sums the values of all props in the object carrying validation values assoiated with each step
+        setProgressValue(sumProgressValue(validationProgress));
+    }, [validationProgress]);
+
+    // Trademark Application Form Steps
+    const steps = [
+        {
+            id: 'Trademark',
+            num: 1,
+            progressValueStart: 0,
+            progressValueEnd: 500,
+        },
+        {
+            id: 'Goods-and-Services',
+            num: 2,
+            progressValueStart: 500,
+            progressValueEnd: 1000,
+        },
+        {
+            id: 'International',
+            num: 3,
+            progressValueStart: 1000,
+            progressValueEnd: 1300,
+        },
+        {
+            id: 'Applicant',
+            num: 4,
+            progressValueStart: 1300,
+            progressValueEnd: 2400,
+        },
+        {
+            id: 'Confirmation',
+            num: 5,
+            progressValueStart: 2400,
+            progressValueEnd: 2600,
+        },
+        {
+            id: 'Payment',
+            num: 6,
+            progressValueStart: 2600,
+            progressValueEnd: 2900,
+        },
+        { id: 'Success', num: 7, progressValueStart: 2900 },
+    ];
+    const { step, navigation } = useStep({
+        steps,
+        initialStep: 0,
+    });
 
     return (
         <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
             <Paper>
                 <Router>
                     <MenuAppBar
+                        navigation={navigation}
+                        step={step}
+                        steps={steps}
+                        progressValue={progressValue}
                         darkMode={darkMode}
                         setDarkMode={setDarkMode}
                         drawerOpen={drawerOpen}
@@ -49,6 +192,15 @@ export default function App() {
                         </Route>
                         <Route path="/application">
                             <TrademarkApplication
+                                navigation={navigation}
+                                step={step}
+                                steps={steps}
+                                info={info}
+                                setInfo={setInfo}
+                                progressValue={progressValue}
+                                setProgressValue={setProgressValue}
+                                validationProgress={validationProgress}
+                                setValidationProgress={setValidationProgress}
                                 darkMode={darkMode}
                                 drawerOpen={drawerOpen}
                             />
--- src/layouts/TrademarkApplication.js
+++ src/layouts/TrademarkApplication.js
@@ -3,118 +3,29 @@ import { Box, Paper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { checkmarksTheme } from '../styles/Themes';
 import bannerImage from '../assets/images/bg-application-yann-allegre.jpg';
-// import bannerImageDark from '../assets/images/bg-dark-application-ricardo-gomez-angel.jpg';
-import bannerImageDark from '../assets/images/bg-dark-application2-asoggetti.jpg';
 import Progress from '../components/TrademarkApplicationPage/Progress';
 import CountryCard from '../components/TrademarkApplicationPage/CountryCard';
 import ConfirmOrder from '../components/TrademarkApplicationPage/ConfirmOrder';
 import PaymentForm from '../components/TrademarkApplicationPage/PaymentForm';
 import TrademarkForm from '../components/TrademarkApplicationPage/TrademarkType/index';
 import GoodsAndServices from '../components/TrademarkApplicationPage/GoodsAndServices';
-import { useStep } from 'react-hooks-helper';
 import Success from '../components/TrademarkApplicationPage/Success';
 import ApplicationInfo from '../components/TrademarkApplicationPage/ApplicationInfo/index';
 import PageLeavePrompt from '../utils/PageLeavePrompt';
 import Footer from '../components/LandingPage/Footer';
-import { sumProgressValue, validateForm } from '../utils/FormValidation';
-// import { checkmarksTheme } from '../styles/Themes';
 
-const TrademarkApplication = ({ darkMode, drawerOpen }) => {
+const TrademarkApplication = ({
+    navigation,
+    step,
+    steps,
+    info,
+    setInfo,
+    progressValue,
+    validationProgress,
+    darkMode,
+    drawerOpen,
+}) => {
     const classes = useStyles();
-
-    // form information
-    const [info, setInfo] = useState({
-        //Application Information
-        individualOrOrganization: 'Individual',
-        firstName: '',
-        lastName: '',
-        organizationName: '',
-        email: '',
-        idDocumentUploaded: false,
-        userStreetAddress: '',
-        userCity: '',
-        userProvince: '',
-        userPostalCode: '',
-        userCountry: '',
-        agreedTermsOfService: false,
-        idName: '',
-
-        //Trademark Type
-        isText: false,
-        isLogo: false,
-        isOther: false,
-        otherTypes: [],
-        characterText: '',
-        fileName: '',
-        file: '',
-
-        // Goods and Services
-        classesSelected: [],
-        termsSelected: [],
-        selectedServiceName: 'DIY',
-        basePrice: 690,
-        amount: 0,
-
-        //International Information
-        filedInOtherCountry: null,
-        countryOfFiling: '',
-        fillingDate: '',
-        fillingNumber: '',
-
-        // Info Confirmed
-        infoConfirmed: false,
-
-        // Payment Information
-        paymentToken: '',
-        paymentCardholderName: '',
-        paymentCreditCardNumber: '',
-        paymentCardExpiryDate: '',
-        paymentCardCVV: '',
-        billingAddressSameAsUser: false,
-        billingStreetAddress: '',
-        billingPostalCode: '',
-        paymentConfirmaed: false,
-    });
-
-    // form validation and progress
-    const [validationProgress, setValidationProgress] = useState({
-        //Application Informarion - 1100 - step 1 end
-        individualOrOrganizationName: 0,
-        firstName: 0,
-        lastName: 0,
-        email: 0,
-        idDocumentUploaded: 0,
-        userStreetAddress: 0,
-        userCity: 0,
-        userProvince: 0,
-        userPostalCode: 0,
-        userCountry: 0,
-        agreedTermsOfService: 0,
-
-        //Trademark Type - 500 - step 2 end
-        trademarkTypeFormCompleted: 0,
-
-        // Goods and Services - 500 - step 3 end
-        amountNotZero: 0,
-
-        //International Information - 300 - step 4 end
-        internationalFilingInfo: 0,
-
-        //Info Confirmed - 200 - step 5 end
-        infoConfirmed: 0,
-
-        // Payment Information - 300 - step 6 end
-        paymentConfirmed: 0,
-    });
-    useEffect(() => {
-        // builds the object carrying validation values assoiated with each step
-        validateForm(info, setValidationProgress);
-    }, [info]);
-    const [progressValue, setProgressValue] = useState(0);
-    useEffect(() => {
-        // sums the values of all props in the object carrying validation values assoiated with each step
-        setProgressValue(sumProgressValue(validationProgress));
-    }, [validationProgress]);
 
     const [currentStep, setCurrentStep] = useState(0);
     useEffect(() => {
@@ -124,61 +35,11 @@ const TrademarkApplication = ({ darkMode, drawerOpen }) => {
     // custon hook, "Discard changes?" on Leave Page (defined in  utils folder)
     const [Prompt, setDirty, setPristine] = PageLeavePrompt();
 
-    //Give each step an id
-    const steps = [
-        {
-            id: 'Trademark',
-            num: 1,
-            progressValueStart: 0,
-            progressValueEnd: 500,
-        },
-        {
-            id: 'Goods-and-Services',
-            num: 2,
-            progressValueStart: 500,
-            progressValueEnd: 1000,
-        },
-        {
-            id: 'International',
-            num: 3,
-            progressValueStart: 1000,
-            progressValueEnd: 1300,
-        },
-        {
-            id: 'Applicant',
-            num: 4,
-            progressValueStart: 1300,
-            progressValueEnd: 2400,
-        },
-        {
-            id: 'Confirmation',
-            num: 5,
-            progressValueStart: 2400,
-            progressValueEnd: 2600,
-        },
-        {
-            id: 'Payment',
-            num: 6,
-            progressValueStart: 2600,
-            progressValueEnd: 2900,
-        },
-        { id: 'Success', num: 7, progressValueStart: 2900 },
-    ];
-
-    //use useStep from hook-helper to navigate the steps
-    const { step, navigation } = useStep({
-        steps,
-        initialStep: 0,
-    });
-
     return (
         <Paper
             className={classes.root}
             style={{
                 backgroundImage: `url(${bannerImage})`,
-                // backgroundImage: `url(${
-                //     darkMode ? bannerImageDark : bannerImage
-                // })`,
                 backgroundSize: 'auto 2900px',
             }}
         >
@@ -200,9 +61,6 @@ const TrademarkApplication = ({ darkMode, drawerOpen }) => {
                         drawerOpen={drawerOpen}
                     />
                     <Box className={classes.hero}>
-                        {/* {console.log(
-                    document.getElementById('parentContainer').offsetHeight
-                )} */}
                         <Typography className={classes.title}>
                             {step.id}
                         </Typography>
@@ -321,7 +179,6 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
     },
     container: {
-        // backgroundColor: checkmarksTheme.bgPrimary,
         display: 'flex',
         width: '100%',
         flexDirection: 'column',
